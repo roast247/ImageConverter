f1dea25bd16f17ad6698d0db4b86167da265e8cb
roast247 <77567356+roast247@users.noreply.github.com>
2021-03-07 22:06:45 -0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,332 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New ComponentFactory.Krypton.Toolkit.KryptonButton()
+        Me.Button2 = New ComponentFactory.Krypton.Toolkit.KryptonButton()
+        Me.Button3 = New ComponentFactory.Krypton.Toolkit.KryptonButton()
+        Me.cmbImageFormat = New ComponentFactory.Krypton.Toolkit.KryptonComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Close_app = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        CType(Me.cmbImageFormat, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.ForeColor = System.Drawing.Color.White
+        Me.TextBox1.Location = New System.Drawing.Point(86, 42)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(185, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox2.ForeColor = System.Drawing.Color.White
+        Me.TextBox2.Location = New System.Drawing.Point(129, 161)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(142, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.TextBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox3.ForeColor = System.Drawing.Color.White
+        Me.TextBox3.Location = New System.Drawing.Point(114, 122)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(143, 20)
+        Me.TextBox3.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(277, 42)
+        Me.Button1.Name = "Button1"
+        Me.Button1.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.SparkleBlue
+        Me.Button1.Size = New System.Drawing.Size(42, 20)
+        Me.Button1.StateCommon.Content.ShortText.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.TabIndex = 3
+        Me.Button1.Values.Text = "..."
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(277, 161)
+        Me.Button2.Name = "Button2"
+        Me.Button2.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.SparkleBlue
+        Me.Button2.Size = New System.Drawing.Size(42, 20)
+        Me.Button2.StateCommon.Content.ShortText.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.TabIndex = 4
+        Me.Button2.Values.Text = "..."
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(128, 196)
+        Me.Button3.Name = "Button3"
+        Me.Button3.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.SparkleBlue
+        Me.Button3.Size = New System.Drawing.Size(95, 25)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Values.Text = "Convert"
+        '
+        'cmbImageFormat
+        '
+        Me.cmbImageFormat.DropDownWidth = 128
+        Me.cmbImageFormat.Location = New System.Drawing.Point(105, 81)
+        Me.cmbImageFormat.Name = "cmbImageFormat"
+        Me.cmbImageFormat.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.SparkleBlue
+        Me.cmbImageFormat.Size = New System.Drawing.Size(118, 21)
+        Me.cmbImageFormat.StateCommon.ComboBox.Back.Color1 = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.cmbImageFormat.StateCommon.ComboBox.Content.Color1 = System.Drawing.Color.White
+        Me.cmbImageFormat.StateCommon.DropBack.Color1 = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.cmbImageFormat.StateCommon.DropBack.Color2 = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.cmbImageFormat.StateCommon.Item.Back.Color1 = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.cmbImageFormat.StateCommon.Item.Back.Color2 = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.cmbImageFormat.StateCommon.Item.Content.ShortText.Color1 = System.Drawing.Color.White
+        Me.cmbImageFormat.StateCommon.Item.Content.ShortText.Color2 = System.Drawing.Color.White
+        Me.cmbImageFormat.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(13, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(67, 15)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Select File:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(13, 85)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(86, 15)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Select Format:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(13, 125)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(95, 15)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "New File Name:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(13, 164)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(110, 15)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Select Destination:"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.InfoText
+        Me.Panel1.Controls.Add(Me.PictureBox2)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Close_app)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(491, 27)
+        Me.Panel1.TabIndex = 12
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackgroundImage = Global.Image_File_Converter.My.Resources.Resources.Convert_image
+        Me.PictureBox2.Image = Global.Image_File_Converter.My.Resources.Resources.Convert_image
+        Me.PictureBox2.Location = New System.Drawing.Point(2, 2)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(25, 25)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 17
+        Me.PictureBox2.TabStop = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(33, 7)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(151, 14)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "Roasting Image Converter"
+        '
+        'Close_app
+        '
+        Me.Close_app.BackColor = System.Drawing.Color.Red
+        Me.Close_app.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Close_app.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Close_app.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Maroon
+        Me.Close_app.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Close_app.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Close_app.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Close_app.ForeColor = System.Drawing.Color.White
+        Me.Close_app.Location = New System.Drawing.Point(464, 0)
+        Me.Close_app.Name = "Close_app"
+        Me.Close_app.Size = New System.Drawing.Size(27, 27)
+        Me.Close_app.TabIndex = 0
+        Me.Close_app.Text = "X"
+        Me.Close_app.UseVisualStyleBackColor = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(336, 196)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(37, 14)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "Width:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(409, 196)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(40, 14)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "Height:"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.PictureBox1.Location = New System.Drawing.Point(339, 42)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(140, 140)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 7
+        Me.PictureBox1.TabStop = False
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Arial", 8.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(370, 196)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(0, 13)
+        Me.Label8.TabIndex = 17
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Arial", 8.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(446, 196)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(0, 13)
+        Me.Label9.TabIndex = 18
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 14.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Gray
+        Me.ClientSize = New System.Drawing.Size(491, 237)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.cmbImageFormat)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.Color.White
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(491, 237)
+        Me.MinimumSize = New System.Drawing.Size(491, 237)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.cmbImageFormat, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button1 As ComponentFactory.Krypton.Toolkit.KryptonButton
+    Friend WithEvents Button2 As ComponentFactory.Krypton.Toolkit.KryptonButton
+    Friend WithEvents Button3 As ComponentFactory.Krypton.Toolkit.KryptonButton
+    Friend WithEvents cmbImageFormat As ComponentFactory.Krypton.Toolkit.KryptonComboBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Close_app As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+End Class
---
+++ Form1.vb
@@ -0,0 +1,165 @@
+﻿Public Class Form1
+
+    Dim SourceFilePath As String = ""
+    Dim DestinationFilePath As String = ""
+    Dim imgFormat As Imaging.ImageFormat
+    Dim Imagesize As Image
+    Private draggable As Boolean
+    Private mouseY As Integer
+    Private mouseX As Integer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        OpenFileDialog1.Title = "Select Image"
+        OpenFileDialog1.FileName = String.Empty
+        OpenFileDialog1.Filter = "Image files|*.bmp;*.gif;*.jpg;*.png;*.jpeg;*.ico;*.tiff;*.exif;*.wmf;*.jif"
+
+        If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+            PictureBox1.Image = Image.FromFile(OpenFileDialog1.FileName)
+            TextBox1.Text = OpenFileDialog1.FileName
+        End If
+
+
+
+        Label8.Text = PictureBox1.Image.Width
+        Label9.Text = PictureBox1.Image.Height
+
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbImageFormat.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim folderDlg As New FolderBrowserDialog
+        If folderDlg.ShowDialog = Windows.Forms.DialogResult.OK Then
+            DestinationFilePath = folderDlg.SelectedPath
+            TextBox2.Text = DestinationFilePath
+        End If
+
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If SourceFilePath.Trim = "" Then
+            MessageBox.Show("Please enter source file path")
+            Return
+        ElseIf DestinationFilePath.Trim = "" Then
+            MessageBox.Show("Please enter destination file path")
+            Return
+        ElseIf TextBox3.Text.Trim = "" Then
+            MessageBox.Show("Please enter file name")
+            Return
+        ElseIf cmbImageFormat.SelectedIndex = 0 Then
+            MessageBox.Show("Please select image format")
+            Return
+        Else
+            Dim LocatioFile As String = DestinationFilePath & "\" & TextBox3.Text
+            Dim strformat As String = cmbImageFormat.Text
+            Select Case strformat
+                Case "Bmp"
+                    imgFormat = Imaging.ImageFormat.Bmp
+                    LocatioFile = LocatioFile & ".bmp"
+                Case "Jpeg"
+                    imgFormat = Imaging.ImageFormat.Jpeg
+                    LocatioFile = LocatioFile & ".jpeg"
+                Case "Png"
+                    imgFormat = Imaging.ImageFormat.Png
+                    LocatioFile = LocatioFile & ".png"
+                Case "Gif"
+                    imgFormat = Imaging.ImageFormat.Gif
+                    LocatioFile = LocatioFile & ".gif"
+                Case "Ico"
+                    imgFormat = Imaging.ImageFormat.Icon
+                    LocatioFile = LocatioFile & ".ico"
+                Case "Tiff"
+                    imgFormat = Imaging.ImageFormat.Tiff
+                    LocatioFile = LocatioFile & ".tiff"
+                Case "Exif"
+                    imgFormat = Imaging.ImageFormat.Exif
+                    LocatioFile = LocatioFile & ".exif"
+            End Select
+            Dim SourceImg As Image
+            SourceImg = Image.FromFile(SourceFilePath)
+
+            SourceImg.Save(LocatioFile, imgFormat)
+            MessageBox.Show("Convert image successfully")
+            PictureBox1.Image = Nothing
+            TextBox1.Clear()
+            TextBox2.Clear()
+            TextBox3.Clear()
+            Label8.Text = ("")
+            Label9.Text = ("")
+            cmbImageFormat.SelectedIndex = 0
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cmbImageFormat.Items.Add("Select")
+        cmbImageFormat.Items.Add("Bmp")
+        cmbImageFormat.Items.Add("Jpeg")
+        cmbImageFormat.Items.Add("Png")
+        cmbImageFormat.Items.Add("Gif")
+        cmbImageFormat.Items.Add("Ico")
+        cmbImageFormat.Items.Add("Tiff")
+        cmbImageFormat.Items.Add("Exif")
+        cmbImageFormat.SelectedIndex = 0
+    End Sub
+
+    Private Sub Close_app_Click(sender As Object, e As EventArgs) Handles Close_app.Click
+        Close()
+    End Sub
+
+    Private Sub Panel1_MouseDown(sender As Object, e As MouseEventArgs) Handles Panel1.MouseDown
+        draggable = True
+        mouseX = Cursor.Position.X - Me.Left
+        mouseY = Cursor.Position.Y - Me.Top
+    End Sub
+
+    Private Sub Panel1_MouseMove(sender As Object, e As MouseEventArgs) Handles Panel1.MouseMove
+        If draggable Then
+            Me.Top = Cursor.Position.Y - mouseY
+            Me.Left = Cursor.Position.X - mouseX
+        End If
+    End Sub
+
+    Private Sub Panel1_MouseUp(sender As Object, e As MouseEventArgs) Handles Panel1.MouseUp
+        draggable = False
+    End Sub
+
+    Private Sub Label5_MouseDown(sender As Object, e As MouseEventArgs) Handles Label5.MouseDown
+        draggable = True
+        mouseX = Cursor.Position.X - Me.Left
+        mouseY = Cursor.Position.Y - Me.Top
+    End Sub
+
+    Private Sub Label5_MouseMove(sender As Object, e As MouseEventArgs) Handles Label5.MouseMove
+        If draggable Then
+            Me.Top = Cursor.Position.Y - mouseY
+            Me.Left = Cursor.Position.X - mouseX
+        End If
+    End Sub
+
+    Private Sub Label5_MouseUp(sender As Object, e As MouseEventArgs) Handles Label5.MouseUp
+        draggable = False
+    End Sub
+
+    Private Sub PictureBox2_MouseDown(sender As Object, e As MouseEventArgs) Handles PictureBox2.MouseDown
+        draggable = True
+        mouseX = Cursor.Position.X - Me.Left
+        mouseY = Cursor.Position.Y - Me.Top
+    End Sub
+
+    Private Sub PictureBox2_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox2.MouseMove
+        If draggable Then
+            Me.Top = Cursor.Position.Y - mouseY
+            Me.Left = Cursor.Position.X - mouseX
+        End If
+    End Sub
+
+    Private Sub PictureBox2_MouseUp(sender As Object, e As MouseEventArgs) Handles PictureBox2.MouseUp
+        draggable = False
+    End Sub
+End Class
